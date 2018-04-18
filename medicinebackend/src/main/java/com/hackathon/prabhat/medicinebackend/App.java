f13a2511a3a32ac616bb7334bde82e4aebf47b07
Prabhat Kumar <prabhat15.cusat@gmail.com>
2018-04-18 18:20:54 +0530

    setup

---
+++ medicinebackend/src/main/java/com/hackathon/prabhat/medicinebackend/App.java
@@ -0,0 +1,13 @@
+package com.hackathon.prabhat.medicinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
